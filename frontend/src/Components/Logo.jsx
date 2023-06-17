4e65bc60389e9c538bde2a3bf459f910c1b46c96
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-06-17 14:50:31 +0200

    feat: logo component

--- frontend/src/Components/Logo.jsx
+++ frontend/src/Components/Logo.jsx
@@ -1,9 +1,13 @@
-const Logo = ({ w, h }) => {
-    return (
-      <div>
-        <img className={`w-32 h-32}`} src="/images/sbff.png" />
+const Logo = ({ mw, mh }) => {
+  return (
+    <div className={`flex lg:flex-col items-center`}>
+      <img src="/images/sbff.png" alt="FulmoFilms" className="w-16 h-16" />
+      <div className="hidden lg:flex lg:flex-col lg:items-center lg:pt-[1px] lg:pb-4">
+        <p className="font-poppins text-base">Mathis Lunot</p>
+        <p className="font-m_poppins text-base">Cinematographer</p>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Logo;
