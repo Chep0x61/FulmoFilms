2bbb826cac23ac8fd1bfa0314510fc17fd5de333
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-08-07 18:53:25 +0200

    feat: 404

--- frontend/src/Pages/NotFound.jsx
+++ frontend/src/Pages/NotFound.jsx
@@ -4,7 +4,10 @@ const NotFound = () => {
     return (
         <div>
             <Navbar />
-            <h1>404 ERROR NOT FOUND</h1>
+            <div className="flex flex-col gap-8 justify-center items-center text-xl lg:text-4xl mt-16">
+                <h1>Nothing to see here</h1>
+                <h2>Here is your 404 Error</h2>
+            </div>
         </div>
     );
 }
