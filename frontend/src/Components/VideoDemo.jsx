018874507206d2b4c2549dd3cb8614e6e41bd28a
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-08-30 03:37:06 +0200

    feat: setup vimeo for demo

--- frontend/src/Components/VideoDemo.jsx
+++ frontend/src/Components/VideoDemo.jsx
@@ -1,13 +1,39 @@
+import React, { useState, useEffect } from 'react';
+
 const VideoDemo = () => {
+    const [videoWidth, setVideoWidth] = useState(992);
+
+    useEffect(() => {
+        const updateVideoDimensions = () => {
+            if (window.innerWidth >= 992) {
+                setVideoWidth(992);
+            } else {
+                setVideoWidth(window.innerWidth);
+            }
+        };
+
+        updateVideoDimensions();
+        window.addEventListener('resize', updateVideoDimensions);
+
+        return () => {
+            window.removeEventListener('resize', updateVideoDimensions);
+        };
+    }, []);
+
     return (
-        <div className="aspect-video lg:max-h-[31rem] max-w-[95%]">
-            <video className="w-full h-full" autoPlay loop muted controls>
-                <source src="/videos/demo.mp4" type="video/mp4" />
-            </video>
+        <div>
+            <iframe
+                title="BandeDémoFulmoFilms"
+                src="https://player.vimeo.com/video/838293783?h=9689c7ee88&autoplay=1&loop=1&title=0"
+                style={{ zIndex: 1 }}
+                width={videoWidth}
+                height={Math.floor((videoWidth * 558) / 992)}
+                frameBorder="0"
+                allow="autoplay; fullscreen; picture-in-picture"
+                allowFullScreen
+            ></iframe>
         </div>
     );
 }
 
 export default VideoDemo;
-
-// 36rem taille video cool mais footer chelou
