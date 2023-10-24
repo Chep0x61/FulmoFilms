4559aa9611b19f58bf83fd49ed434b84169a21bd
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-10-24 22:04:22 +0200

    fix: wip message

--- frontend/src/Components/Wip.jsx
+++ frontend/src/Components/Wip.jsx
@@ -2,7 +2,7 @@ const Wip = () => {
   return (
     <div className="pt-32 pb-16 flex justify-center items-center text-center text-xl lg:text-3xl">
       <p>
-        This page is still under development!<br />Please come back later to discover it.
+      Nothing to show you right now <br /> check back later, there might be something new to see here.
       </p>
     </div>
   );
