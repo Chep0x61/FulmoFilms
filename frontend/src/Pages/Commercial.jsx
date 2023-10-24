a4f6bb8328a6ce6c8b4fe5071a8febcadca6629a
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-10-24 22:05:02 +0200

    feat: AFPA vid

--- frontend/src/Pages/Commercial.jsx
+++ frontend/src/Pages/Commercial.jsx
@@ -1,12 +1,21 @@
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
-import Wip from "../Components/Wip";
+
+import VimeoCustomVideo from "../Components/VimeoCustomVideo";
 
 const Commercials = () => {
     return (
         <div>
             <Navbar />
-            <Wip />
+            <div className="mt-16 flex justify-center items-center lg:m-0">
+            <VimeoCustomVideo
+                title="AFPA - Mathis Lunot"
+                link="836153003"
+                autoplay={false}
+                loop={false}
+                muted={false}
+            />
+            </div>
             <Footer />
         </div>
     );
