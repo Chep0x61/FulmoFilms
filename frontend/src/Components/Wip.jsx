17a73a52affd39a41878aac958904d00bc7471b6
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-11-22 23:36:34 +0800

    test: add smiley to test autodeploy

--- frontend/src/Components/Wip.jsx
+++ frontend/src/Components/Wip.jsx
@@ -2,7 +2,7 @@ const Wip = () => {
   return (
     <div className="pt-32 pb-16 flex justify-center items-center text-center text-xl lg:text-3xl">
       <p>
-      Nothing to show you right now <br /> check back later, there might be something new to see here.
+      Nothing to show you right now <br /> check back later, there might be something new to see here. :)
       </p>
     </div>
   );
