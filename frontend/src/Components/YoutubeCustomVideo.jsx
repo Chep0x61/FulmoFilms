54849ee384ea82deedb2c7393ebd3dfffe6c9a59
Thibault Thuillier <thibault.thuillier@epitech.eu>
2024-09-26 21:01:55 +0200

    feat: youtube video component

--- frontend/src/Components/YoutubeCustomVideo.jsx
+++ frontend/src/Components/YoutubeCustomVideo.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-const VideoDemo = () => {
+const YoutubeCustomVideo = ({ title, videoId, autoplay, loop, muted, title_layout }) => {
     const [videoWidth, setVideoWidth] = useState(992);
 
     useEffect(() => {
@@ -20,15 +20,17 @@ const VideoDemo = () => {
         };
     }, []);
 
+    const videoSrc = `https://www.youtube.com/embed/${videoId}?autoplay=${autoplay ? 1 : 0}&loop=${loop ? 1 : 0}&mute=${muted ? 1 : 0}&showinfo=${title_layout ? 1 : 0}&playlist=${videoId}`;
+
     return (
         <div>
             <iframe
-                title="Mathis Lunot's showreel"
-                src="https://player.vimeo.com/video/838293783?autoplay=1&loop=1&title=0&muted=1"
+                title={title || "A Mathis Lunot's YouTube video"}
+                src={videoSrc}
                 style={{ zIndex: 1 }}
                 width={videoWidth}
                 height={Math.floor((videoWidth * 558) / 992)}
-                frameborder="0"
+                frameBorder="0"
                 allow="autoplay; fullscreen; picture-in-picture"
                 allowFullScreen
             ></iframe>
@@ -36,4 +38,4 @@ const VideoDemo = () => {
     );
 }
 
-export default VideoDemo;
+export default YoutubeCustomVideo;
