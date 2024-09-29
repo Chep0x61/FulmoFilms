687bbc73ed006dc82a083fe23d4ee8efc9aec2c7
Thibault Thuillier <thibault.thuillier@epitech.eu>
2024-09-29 21:10:25 +0200

    feat: get year with Date

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,6 +1,8 @@
 import CircleBadge from "./CircleBadge";
 
 const Footer = () => {
+    const currentYear = new Date().getFullYear();
+
     return (
         <div className="flex flex-col justify-center items-center pt-8">
             <div className="flex gap-x-1">
@@ -11,7 +13,7 @@ const Footer = () => {
                 <CircleBadge src="/icons/socials/linkedin.png" alt="LinkedInMathisLunot" href="https://www.linkedin.com/in/mathis-lunot/"/>
             </div>
             <a className="my-4 text-sm md:text-md lg:text-base hover:text-[#BDBDBD]" href="https://github.com/Chep0x61" target="_blank" rel="noreferrer">
-                <p>© FulmoFilms 2023 - Made with ❤️ by Chep0x61 !</p>
+                <p>© FulmoFilms {currentYear} - Made with ❤️ by Chep0x61 !</p>
             </a>
         </div>
     );
